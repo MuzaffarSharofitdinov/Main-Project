556347d01e1abfe6c748f60eac765ab64f2d911b
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2025-05-07 16:24:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 secret_key = 'cdd303f0-d70a-4e36-a9f7-f94a14b59942'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@database-01.czmewsoo2jv0.ap-northeast-2.rds.amazonaws.com:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://<user>:<password>@<host>:<port>/<database>'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secret_key
 
